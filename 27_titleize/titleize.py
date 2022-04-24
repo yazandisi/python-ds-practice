662480748bbf5772554f4db15b0f6e9328f5eab0
Yazan Eldisi <yazandisi@gmail.com>
2022-04-24 14:55:15 -0500

    All problems solved

---
+++ 27_titleize/titleize.py
@@ -0,0 +1,13 @@
+def titleize(phrase):
+    """Return phrase in title case (each word capitalized).
+
+        >>> titleize('this is awesome')
+        'This Is Awesome'
+
+        >>> titleize('oNLy cAPITALIZe fIRSt')
+        'Only Capitalize First'
+    """
+    return phrase.title()
+
+    # by hand:
+    # return ' '.join([s.capitalize() for s in phrase.split(' ')])
